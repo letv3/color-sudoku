0ecea9f2fc3bc0373c0952f8c4319a3f852a0c13
Oleksandr Lytvyn <oleksandr.lytvyn@student.tuke.sk>
2019-05-09 13:38:34 +0200

    poject

--- ColorSudoku_WEB/Controllers/SudokuController.cs
+++ ColorSudoku_WEB/Controllers/SudokuController.cs
@@ -70,78 +70,92 @@ namespace ColorSudoku_WEB.Controllers
 
 
 
-        //[Route("sudoku/login")]
-        [HttpPost("/sudoku/login")]
-        public IActionResult Login(string login)
-        {
-
-            loggedUser = login;
-
-            field = (Field)HttpContext.Session.GetObject("field");
-            HttpContext.Session.SetObject("field", field);
-
-            updateModel(model);
-
-            return View("Index", model);
-        }
-
-        [Route("/sudoku/logout")]
-        public IActionResult logout()
+        [HttpPost("/sudoku/addUserData")]
+        public IActionResult addUserData([FromForm] string name, [FromForm] string comment, [FromForm] int rating)
         {
             var field = (Field)HttpContext.Session.GetObject("field");
-            HttpContext.Session.SetObject("field", field);
-            loggedUser = null;
-            updateModel(model);
 
-            return RedirectToAction("Index", model);
-        }
-
-        [HttpPost("/sudoku/addName")]
-        public IActionResult addName([FromForm] string name)
-        {
-            var field = (Field)HttpContext.Session.GetObject("field");
-            loggedUser = name;
-            _scoreService.AddScore(new Score
-            {
-                Name = loggedUser,
-                Points = field.Score,
-                TimeOfScore = DateTime.Now
-            });
-            HttpContext.Session.SetObject("field", field);
-            updateModel(model);
+            _scoreService.AddScore(new Score{Name=name, Points = field.Score, TimeOfScore = DateTime.Now});
+            _commentService.AddComment(new Comment{Name=name,Message = comment, TimeOFComment = DateTime.Now});
+            _ratingService.AddRating(new Rating{Name=name, Mark=rating, TimeOfRating = DateTime.Now});
 
             return RedirectToAction("Index", model);
         }
 
 
 
-        [HttpPost("/sudoku/addComment")]
-        public IActionResult addComment([FromForm] string comment)
-        {
-            _commentService.AddComment(new Comment
-            {
-                Name = loggedUser,
-                Message = comment,
-                TimeOFComment = DateTime.Now
-            });
-            updateModel(model);
-            return RedirectToAction("Index", model);
-            
-        }
 
-        [HttpPost("/sudoku/addRating")]
-        public IActionResult addRating([FromForm] int rating)
-        {
-            _ratingService.AddRating(new Rating
-            {
-                Name = loggedUser,
-                Mark = rating,
-                TimeOfRating = DateTime.Now
-            });
-            updateModel(model);
-            return RedirectToAction("Index", model);
-           
-        }
+        ////[Route("sudoku/login")]
+        //[HttpPost("/sudoku/login")]
+        //public IActionResult Login(string login)
+        //{
+
+        //    loggedUser = login;
+
+        //    field = (Field)HttpContext.Session.GetObject("field");
+        //    HttpContext.Session.SetObject("field", field);
+
+        //    updateModel(model);
+
+        //    return View("Index", model);
+        //}
+
+        //[Route("/sudoku/logout")]
+        //public IActionResult logout()
+        //{
+        //    var field = (Field)HttpContext.Session.GetObject("field");
+        //    HttpContext.Session.SetObject("field", field);
+        //    loggedUser = null;
+        //    updateModel(model);
+
+        //    return RedirectToAction("Index", model);
+        //}
+
+        //[HttpPost("/sudoku/addName")]
+        //public IActionResult addName([FromForm] string name)
+        //{
+        //    var field = (Field)HttpContext.Session.GetObject("field");
+        //    loggedUser = name;
+        //    _scoreService.AddScore(new Score
+        //    {
+        //        Name = loggedUser,
+        //        Points = field.Score,
+        //        TimeOfScore = DateTime.Now
+        //    });
+        //    HttpContext.Session.SetObject("field", field);
+        //    updateModel(model);
+
+        //    return RedirectToAction("Index", model);
+        //}
+
+
+
+        //[HttpPost("/sudoku/addComment")]
+        //public IActionResult addComment([FromForm] string comment)
+        //{
+        //    _commentService.AddComment(new Comment
+        //    {
+        //        Name = loggedUser,
+        //        Message = comment,
+        //        TimeOFComment = DateTime.Now
+        //    });
+        //    updateModel(model);
+        //    return RedirectToAction("Index", model);
+
+        //}
+
+        //[HttpPost("/sudoku/addRating")]
+        //public IActionResult addRating([FromForm] int rating)
+        //{
+        //    _ratingService.AddRating(new Rating
+        //    {
+        //        Name = loggedUser,
+        //        Mark = rating,
+        //        TimeOfRating = DateTime.Now
+        //    });
+        //    updateModel(model);
+        //    return RedirectToAction("Index", model);         
+        //}
 
 
         private void updateModel(SudokuModel model)
